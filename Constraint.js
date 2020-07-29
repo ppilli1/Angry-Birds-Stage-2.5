48369d8e7ab7ecddfae00287127b816839dd4df1
starninjaz8055 <67479423+starninjaz8055@users.noreply.github.com>
2020-07-29 13:39:55 -0400

    Add files via upload

---
+++ Constraint.js
@@ -0,0 +1,18 @@
+class Chain {
+    constructor(BodyA,BodyB) {
+        var options = {
+            bodyA: BodyA,
+            bodyB: BodyB,
+            stiffness: 0.04,
+            length: 10
+        }
+    this.chain = Constraint.create(options);
+    World.add(world,this.chain);
+    }
+    
+    display() {
+    strokeWeight(3);
+    line(this.chain.bodyA.position.x,this.chain.bodyA.position.y,this.chain.bodyB.position.x,this.chain.bodyB.position.y);
+    
+    }
+}
